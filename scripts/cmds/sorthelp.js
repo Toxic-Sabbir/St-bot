395673a9a3f1069cb212c9641c663fd95badaeca
Sabbir Hossain <79851894+Toxic-Sabbir@users.noreply.github.com>
2026-09-25 12:53:11 +0600

    Update sorthelp.js

--- scripts/cmds/sorthelp.js
+++ scripts/cmds/sorthelp.js
@@ -9,7 +9,7 @@ module.exports = {
 			vi: "Sắp xếp danh sách help",
 			en: "Sort help list"
 		},
-		category: "image",
+		category: "ADMIN & BOT CONTROL",
 		guide: {
 			en: "{pn} [name | category]"
 		}
