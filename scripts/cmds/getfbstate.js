ba689bb5cf3b25a961312b50709e71a395c80a46
Sabbir Hossain <79851894+Toxic-Sabbir@users.noreply.github.com>
2026-09-25 12:23:58 +0600

    Update getfbstate.js

--- scripts/cmds/getfbstate.js
+++ scripts/cmds/getfbstate.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Lấy fbstate hiện tại",
 			en: "Get current fbstate"
 		},
-		category: "owner",
+		category: "ADMIN & BOT CONTROL",
 		guide: {
 			en: "   {pn}: get fbstate (appState)\n"
 				+ "   {pn} [cookies|cookie|c]: get fbstate with cookies format\n"
