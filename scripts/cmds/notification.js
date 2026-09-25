177e2af0dfde33497ff8d795c382051deb0b9b7f
Sabbir Hossain <79851894+Toxic-Sabbir@users.noreply.github.com>
2026-09-25 12:36:17 +0600

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Gửi thông báo từ admin đến all box",
 			en: "Send notification from admin to all box"
 		},
-		category: "owner",
+		category: "ADMIN & BOT CONTROL",
 		guide: {
 			en: "{pn} <tin nhắn>"
 		},
