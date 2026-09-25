84f1100cf9ba24fb7895449da6ab0e1a2b56b2c3
Sabbir Hossain <79851894+Toxic-Sabbir@users.noreply.github.com>
2026-09-25 12:13:54 +0600

    Update cmd.js

--- scripts/cmds/cmd.js
+++ scripts/cmds/cmd.js
@@ -35,7 +35,7 @@ module.exports = {
 			vi: "Quản lý các tệp lệnh của bạn",
 			en: "Manage your command files"
 		},
-		category: "owner",
+		category: "ADMIN & BOT CONTROL",
 		guide: {
 			vi: "   {pn} load <tên file lệnh>"
 				+ "\n   {pn} loadAll"
