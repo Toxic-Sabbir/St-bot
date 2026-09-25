170bfcd80a6845cab62bab04a062b4967d2457d4
Sabbir Hossain <79851894+Toxic-Sabbir@users.noreply.github.com>
2026-09-25 12:48:17 +0600

    Update sendnoti.js

--- scripts/cmds/sendnoti.js
+++ scripts/cmds/sendnoti.js
@@ -11,7 +11,7 @@ module.exports = {
 			vi: "Tạo và gửi thông báo đến các nhóm do bạn quản lý",
 			en: "Create and send notification to groups that you manage"
 		},
-		category: "box chat",
+		category: "ADMIN & BOT CONTROL",
 		guide: {
 			vi: "   {pn} create <groupName>: Tạo một group noti (notification) mới với tên gọi <groupName>"
 				+ "\n   Ví dụ:\n    {pn} create TEAM1"
