b8a4a927adebbb6d5f876a02584db08b49cd2c3f
Sabbir Hossain <79851894+Toxic-Sabbir@users.noreply.github.com>
2026-09-25 13:00:34 +0600

    Update warn.js

--- scripts/cmds/warn.js
+++ scripts/cmds/warn.js
@@ -11,7 +11,7 @@ module.exports = {
 			vi: "cảnh cáo thành viên trong nhóm, đủ 3 lần ban khỏi box",
 			en: "warn member in group, if they have 3 warns, they will be banned"
 		},
-		category: "box chat",
+		category: "ADMIN & BOT CONTROL",
 		guide: {
 			vi: "   {pn} @tag <lý do>: dùng cảnh cáo thành viên"
 				+ "\n   {pn} list: xem danh sách những thành viên đã bị cảnh cáo"
